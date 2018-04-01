c36717aaa77f3eafe0a308eea8da1d708d5b24db
prath031 <prameetrathod@192.168.2.22>
2018-04-01 10:49:14 -0400

    test jenkins

---
+++ src/test/java/com/jenkins/integration/JenkinsTest.java
@@ -0,0 +1,32 @@
+package com.jenkins.integration;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	public void test1() {
+	 System.out.println("test 1 executed");	
+      Assert.fail();
+	}
+	
+	@Test
+	public void test2() {
+	 System.out.println("test 2 executed");	
+      //Assert.fail();
+	}
+	
+	@Test
+	public void test3() {
+	 System.out.println("test 3 executed");	
+      Assert.fail();
+	}
+	
+	@Test
+	public void test4() {
+	 System.out.println("test 4 executed");	
+      //Assert.fail();
+	}
+
+}
